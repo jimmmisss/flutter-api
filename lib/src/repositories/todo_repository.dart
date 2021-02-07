4837fd6cddbcf7643c894e81cbf5f304242c0c6a
Wesley Pereira <jimmmisss@gmail.com>
2021-02-06 22:42:42 -0300

    Comitando projeto de teste que consome api externa.

---
+++ lib/src/repositories/todo_repository.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_api/src/models/todo_model.dart';
+
+class TodoRepository {
+  Dio dio;
+  final url = 'https://jsonplaceholder.typicode.com/todos';
+
+  TodoRepository([Dio client]) {
+    if (client == null) {
+      this.dio = Dio();
+    } else {
+      this.dio = client;
+    }
+  }
+
+  Future<List<TodoModel>> fetchTodos() async {
+    final response = await dio.get(url);
+    final list = response.data as List;
+
+    List<TodoModel> todos = [];
+
+    for (var json in list) {
+      final todo = TodoModel.fromJson(json);
+      todos.add(todo);
+    }
+    return todos;
+  }
+}
