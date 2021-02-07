0ec01055881aee9527fedd6d4a07606fe992069e
Wesley Pereira <jimmmisss@gmail.com>
2021-02-07 00:23:33 -0300

    Criando controller

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -1,1 +1,14 @@
-class HomeController {}
+import 'package:flutter_api/src/models/todo_model.dart';
+import 'package:flutter_api/src/repositories/todo_repository.dart';
+
+class HomeController {
+  List<TodoModel> todos = [];
+  final TodoRepository _repository;
+
+  HomeController([TodoRepository repository])
+      : _repository = repository ?? TodoRepository();
+
+  Future start() async {
+    todos = await _repository.fetchTodos();
+  }
+}
---
+++ test/src/controllers/home_controllers_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_api/src/controllers/home_controller.dart';
+import 'package:flutter_api/src/models/todo_model.dart';
+import 'package:flutter_api/src/repositories/todo_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class TodoRepositoryMock extends Mock implements TodoRepository {}
+
+main() {
+  final repository = TodoRepositoryMock();
+  final controller = HomeController();
+
+  test('Deve preencher variável todos', () async {
+    when(repository.fetchTodos()).thenAnswer((_) async => [TodoModel()]);
+    await controller.start();
+    expect(controller.todos.isNotEmpty, true);
+  });
+}
